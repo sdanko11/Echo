f5985c2c2584095b4f8fa8d2744efa4ce3131ffa
steve <stevedanko1@gmail.com>
2013-11-23 18:59:53 -0500

    added a few more automatic responses

--- echo.rb
+++ echo.rb
@@ -1,8 +1,15 @@
 require 'pry'
 
 def playback(what_will_playback)
+  if what_will_playback.downcase == 'nothing!'
+    puts 'Ok, fine!'
+elsif what_will_playback.downcase == 'i have a lot to say'
+    puts 'I dont have time for that right now'
+elsif what_will_playback.downcase != 'nothing' || what_will_playback.downcase != 'i have alot to say'
   puts what_will_playback
+  end
 end
+
 
 
 puts 'What do you want to say'
