e543716a5f74926d0b9e6e714488bb8edeb65a07
steve <stevedanko1@gmail.com>
2013-11-23 19:56:21 -0500

    added functionality to store multiple responses

--- echo.rb
+++ echo.rb
@@ -1,13 +1,29 @@
 require 'pry'
+@multiple_responses = []
 
 def playback(what_will_playback)
   if what_will_playback.downcase == 'nothing!'
     puts 'Ok, fine!'
 elsif what_will_playback.downcase == 'i have a lot to say'
-    puts 'I dont have time for that right now'
+    puts 'Ok lets here it'
+    while what_will_playback.downcase != 'done'
+      what_will_playback=gets.chomp
+      if what_will_playback != 'done'
+       @multiple_responses << what_will_playback
+     end
+    end
+    how_many_responsses = @multiple_responses.count
+    first_repsonse = @multiple_responses.shift
+    last_response = @multiple_responses.pop
+    puts "First you said: #{first_repsonse}"
+      @multiple_responses.each do |statement|
+        puts "Then you said: #{statement}"
+      end
+      puts "Finialy you said: #{last_response}"
+      puts "Phew glad you got all #{how_many_responsses} of those things off your chests"
 elsif what_will_playback.downcase != 'nothing' || what_will_playback.downcase != 'i have alot to say'
   puts what_will_playback
-  end
+end
 end
 
 
@@ -15,4 +31,3 @@ end
 puts 'What do you want to say'
 answer=gets.chomp
 playback(answer)
-
