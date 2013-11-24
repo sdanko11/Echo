6b989a0bc55ac8241298c00af409e77836aa5a17
steve <stevedanko1@gmail.com>
2013-11-23 21:25:51 -0500

    added in the ability to read from a file

--- echo.rb
+++ echo.rb
@@ -1,33 +1,75 @@
 require 'pry'
 @multiple_responses = []
 
-def playback(what_will_playback)
-  if what_will_playback.downcase == 'nothing!'
+def nothing_to_say
     puts 'Ok, fine!'
-elsif what_will_playback.downcase == 'i have a lot to say'
+end
+
+def repeat_the_statement(statement)
+  puts statement
+end
+
+def has_alot_to_say
     puts 'Ok lets here it'
-    while what_will_playback.downcase != 'done'
-      what_will_playback=gets.chomp
-      if what_will_playback != 'done'
-       @multiple_responses << what_will_playback
+    statement = gets.chomp
+    while statement.downcase != 'done'
+      statement = gets.chomp
+      if statement != 'done'
+       @multiple_responses << statement
      end
     end
     how_many_responsses = @multiple_responses.count
     first_repsonse = @multiple_responses.shift
     last_response = @multiple_responses.pop
     puts "First you said: #{first_repsonse}"
-      @multiple_responses.each do |statement|
-        puts "Then you said: #{statement}"
+      @multiple_responses.each do |line|
+        puts "Then you said: #{line}"
       end
       puts "Finialy you said: #{last_response}"
       puts "Phew glad you got all #{how_many_responsses} of those things off your chests"
-elsif what_will_playback.downcase != 'nothing' || what_will_playback.downcase != 'i have alot to say'
-  puts what_will_playback
-end
 end
 
+def has_a_speech
+    puts "Ok where can I find what you want to say?"
+    file_name = gets.chomp
+    puts "Loading #{file_name}..."
+    if file_name == 'speech.txt'
+      File.open("#{file_name}", "r").each_line do |line|
+      @multiple_responses << line
+      end
+    else file_name != 'speech.txt'
+    puts "I could not find the file #{file_name}"
+end
+  if @multiple_responses.count > 0
+    how_many_responsses = @multiple_responses.count
+    first_repsonse = @multiple_responses.shift
+    last_response = @multiple_responses.pop
+    puts "First you said: #{first_repsonse}"
+    @multiple_responses.each do |line|
+        puts "Then you said: #{line}"
+      end
+   puts "Finialy you said: #{last_response}"
+   puts "Phew glad you got all #{how_many_responsses} of those things off your chests" 
+  end
+end
 
 
 puts 'What do you want to say'
 answer=gets.chomp
-playback(answer)
+
+if answer.downcase == 'nothing'
+  nothing_to_say
+elsif answer.downcase == 'i have alot to say'
+  has_alot_to_say
+elsif answer.downcase == 'i have something prepared'
+  has_a_speech
+else 
+  repeat_the_statement(answer)
+end
+  
+
+
+
+
+
+
